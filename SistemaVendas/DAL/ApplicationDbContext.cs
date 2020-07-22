484875102754478d18cf38866c2494f3b8c72eca
Guilherme Duarte <guilherme.duarte@mirasoft.com.br>
2020-07-21 22:35:07 -0300

    Aula 13 - DbContext (continuar)

---
+++ SistemaVendas/DAL/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaVendas.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaVendas.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<Produto> Produto { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Cliente> Cliente { get; set; }
+        public DbSet<Venda> Venda { get; set; }
+        public DbSet<VendaProdutos> VendaProdutos { get; set; }
+
+        
+    }
+}
