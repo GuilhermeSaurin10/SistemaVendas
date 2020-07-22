794d405ce5b031c12ddd6fb55df3c3ab461be893
Guilherme Duarte <guilherme.duarte@mirasoft.com.br>
2020-07-21 22:24:56 -0300

    Aula 12 - Classes de Entidades do Sistema

---
+++ SistemaVendas/Entidades/VendaProdutos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaVendas.Entidades
+{
+    public class VendaProdutos
+    {
+        public int CodigoVenda { get; set; }
+        public int CodigoProduto { get; set; }
+        public double Quantidade { get; set; }
+        public decimal ValorUnitario { get; set; }
+        public decimal ValorTotal { get; set; }
+        public Produto Produto { get; set; }
+        public Venda Venda { get; set; }
+
+    }
+}
