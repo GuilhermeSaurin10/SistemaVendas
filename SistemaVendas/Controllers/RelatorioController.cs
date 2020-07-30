4fe5b7ad16cbbd1dddf500f0a5bbb969eab2e3f4
Guilherme Duarte <guilherme.duarte@mirasoft.com.br>
2020-07-30 17:00:24 -0300

    Aula 22 - Relatório de Vendas em Gráfico (Chart JS)

---
+++ SistemaVendas/Controllers/RelatorioController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SistemaVendas.DAL;
+using SistemaVendas.Models;
+
+namespace SistemaVendas.Controllers
+{
+    public class RelatorioController : Controller
+    {
+        protected ApplicationDbContext mContext;
+
+        public RelatorioController(ApplicationDbContext context)
+        {
+            mContext = context;
+        }
+
+
+        public IActionResult Grafico()
+        {
+            var lista = mContext.VendaProdutos
+                .GroupBy(x => x.CodigoProduto)
+                .Select(y => new GraficoViewModel
+                {
+                    CodigoProduto = y.First().CodigoProduto,
+                    Descricao = y.First().Produto.Descricao,
+                    TotalVendido = y.Sum(z => z.Quantidade)
+                }).ToList();
+
+            string valores = string.Empty;
+            string labels = string.Empty;
+            string cores = string.Empty;
+
+            var random = new Random();
+
+            for (int i = 0; i < lista.Count; i++)
+            {
+                valores += lista[i].TotalVendido.ToString() + ",";
+                labels += "'" + lista[i].Descricao.ToString() + ",";
+                cores += "'" + string.Format("#{0:x6}",random.Next(0x1000000)) + "',";
+            }
+
+            ViewBag.Valores = valores;
+            ViewBag.Labels = labels;
+            ViewBag.Cores = cores;
+
+
+
+            return View();
+        }
+    }
+}
