b3de1bd8d9d7797c34c891f7ee1ced527697df52
Guilherme Duarte <guilherme.duarte@mirasoft.com.br>
2020-07-31 12:16:43 -0300

    Aula 23 - Login de Usuário (1/2)

---
+++ SistemaVendas/Controllers/LoginController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaVendas.DAL;
+using SistemaVendas.Helpers;
+using SistemaVendas.Models;
+
+namespace SistemaVendas.Controllers
+{
+    public class LoginController : Controller
+    {
+        protected ApplicationDbContext mContext;
+
+        public LoginController(ApplicationDbContext context)
+        {
+            mContext = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(LoginViewModel model)
+        {
+            ViewData["ErroLogin"] = string.Empty;
+
+            if (ModelState.IsValid)
+            {
+              var Senha = Criptografia.GetMd5Hash(model.Senha);
+              var usuario = mContext.Usuario.Where(x => x.Email == model.Email && x.Senha == Senha).FirstOrDefault();
+
+                if (usuario == null)
+                {
+                    ViewData["ErroLogin"] = "O Email ou senha informado não existe no sistema!";
+                    return View(model);
+                }
+                else
+                {
+                    //colocar os dados do usuário na sessão
+                    RedirectToAction("Index", "Home");
+                }
+
+                return View(model);
+            }
+            else
+            {
+                
+            }
+
+            return View(model);
+        }
+    }
+}
--- SistemaVendas/Startup.cs
+++ SistemaVendas/Startup.cs
@@ -61,7 +61,7 @@ namespace SistemaVendas
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Login}/{action=Index}/{id?}");
             });
         }
     }
